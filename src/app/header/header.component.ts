30a4a29a44fc737a9a630ac1cf5c08c245e0037a
klajdm <klajdimurati3@gmail.com>
2026-05-15 00:20:20 +0200

    integrate Tailwind CSS and update project configuration

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,9 +4,9 @@ import { GhButtonModule } from '@ctrl/ngx-github-buttons';
 @Component({
   selector: 'app-header',
   template: `
-    <header class="header mt-5 text-center">
+    <header class="header mt-20 text-center">
       <h1>Angular Notitia</h1>
-      <p style="color: #777" class="mb-1">Easy Toasts for Angular</p>
+      <p style="color: #777" class="mb-1 mt-0">Easy Toasts for Angular</p>
       <gh-button user="klajdm" repo="ngx-notitia" [count]="true"></gh-button>
     </header>
   `,
